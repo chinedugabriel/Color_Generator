60e99e6e3d409cd7ff8cacecd9b572acaae161e5
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-22 21:12:17 +0100

    added files and created the structure

---
+++ Color_generator .js
@@ -0,0 +1,16 @@
+
+//     let colorOutput = document.getElementById("ColorOutput");
+//     let redRange = document.getElementById("red").value;
+//     let greenRange = document.getElementById("green");
+//     let blueRange = document.getElementById("blue");
+
+
+// function colorGenerator(){  
+      
+//       colorOutput.innHTML = redRange;
+//       // alert (redRange.input);
+//     }
+    
+//     ColorOutput.addEventListener("input", colorGenerator);
+
+
