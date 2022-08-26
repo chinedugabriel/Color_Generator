6bacd86ad05291809762cbd94cc320e868f6a62c
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-26 01:16:37 +0100

    added function to view the value selected for each range input when it change including opacity value

--- Color_generator.js
+++ Color_generator.js
@@ -15,7 +15,7 @@ let inputRedRgb = document.getElementById("set-red");
 let view_inputGreenRgb = document.getElementById("view-green-value-rgb");
 let inputGreenRgb = document.getElementById("set-green");
 
-let viewBlueRgb = document.getElementById("view-blue-value-rgb");
+let view_BlueRgb = document.getElementById("view-blue-value-rgb");
 let inputBlueRgb = document.getElementById("set-blue");
 
 
@@ -42,7 +42,7 @@ let view_opacityControl = document.getElementById("view-opacity-value");
 let opacityControl = document.getElementById("opacity");
 
 // view general color code 
-let view_generalColorCode = document.getElementById("color-code-value");
+let view_generalColorCode = document.getElementById("color-code-value-p");
 
 // add color to pallet section
 let btn_addColorToSection = document.getElementById("add-to-pallet");
@@ -50,35 +50,67 @@ let btn_addColorToSection = document.getElementById("add-to-pallet");
 // color pallet section added for later
 let view_color_added = document.getElementById("color-add-result-section");
 
-// function to toggle controls between RGB, HEX, HSL
+// function to chose color controls between RGB, HEX, HSL
+btn_RGB.style.backgroundColor = "#005cc8";
+btn_RGB.style.color = "white";
 function showRGB(){
     setColorControl_RGB.style.display = "flex";
     setColorControl_HEX.style.display = "none";
     setColorControl_HSL.style.display = "none";
+    btn_RGB.style.backgroundColor = "#005cc8";
+    btn_RGB.style.color = "white";
+    //  below code remove's the background color for btn_HSL and btn_HEX
+    btn_HSL.style.backgroundColor = "";
+    btn_HSL.style.color = "black";
+    // 
+    btn_HEX.style.backgroundColor = "";
+    btn_HEX.style.color = "black";
+
 }
+// btn call the function to show RGB control section.
 btn_RGB.addEventListener("click", showRGB);
+
+// live view function to see the color that's been selected
+function RGB_liveView(){
+    view_inputRedRgb.innerHTML = inputRedRgb.value;
+    view_inputGreenRgb.innerHTML = inputGreenRgb.value;
+    view_opacityControl.innerHTML = opacityControl.value ;
+    view_BlueRgb.innerHTML = inputBlueRgb.value;
+    live_view_section.style.backgroundColor = `rgba(${inputRedRgb.value}, ${inputGreenRgb.value}, ${inputBlueRgb.value}, ${opacityControl.value/100})`;
+
+    view_generalColorCode.innerHTML = `rgba(${inputRedRgb.value}, ${inputGreenRgb.value}, ${inputBlueRgb.value}, ${opacityControl.value/100})`;
+}
+inputRedRgb.addEventListener("change", RGB_liveView);
+inputGreenRgb.addEventListener("change", RGB_liveView);
+inputBlueRgb.addEventListener("change", RGB_liveView);
+opacityControl.addEventListener("change", RGB_liveView);
 
 function showHSL(){
     setColorControl_RGB.style.display = "none";
     setColorControl_HEX.style.display = "none";
     setColorControl_HSL.style.display = "flex";
-    
+    btn_HSL.style.backgroundColor = "#005cc8";
+    btn_HSL.style.color = "white";
+    //  below code remove's the background color for btn_RGB and btn_HEX
+    btn_HEX.style.backgroundColor = "";
+    btn_HEX.style.color = "black";
+    btn_RGB.style.backgroundColor = "";
+    btn_RGB.style.color = "black";
 }
 btn_HSL.addEventListener("click", showHSL);
 
 function showHEX(){
     setColorControl_RGB.style.display = "none";
     setColorControl_HEX.style.display = "flex";
-    // btn_HEX.style.backgroundColor = "blue";
     setColorControl_HSL.style.display = "none";
+    btn_HEX.style.backgroundColor = "#005cc8";
+    btn_HEX.style.color = "white";
+    // below code remove's the background color for btn_RGB and btn_HSL
+    btn_RGB.style.backgroundColor = "";
+    btn_RGB.style.color = "black";
+    btn_HSL.style.backgroundColor = "";
+    btn_HSL.style.color = "black";
 }
 btn_HEX.addEventListener("click", showHEX);
 
-// function colorGenerator(){  
-      
-//       colorOutput.innHTML = redRange;
-//       // alert (redRange.input);
-//     }
-    
-//     ColorOutput.addEventListener("change", colorGenerator);
 
